d9751450e8a20b3bd1d60df8f26c6c49c65596c6
Brendan Bosman <bhbosman@gmail.com>
2022-08-02 21:52:20 +0200

    refactor

--- fx/PubSub/ProvidePubSub.go
+++ fx/PubSub/ProvidePubSub.go
@@ -10,15 +10,22 @@ func ProvidePubSub(name string) fx.Option {
 	return fx.Provide(
 		fx.Annotated{
 			Name: name,
-			Target: func(lifecycle fx.Lifecycle) *pubsub.PubSub {
+			Target: func(
+				params struct {
+					fx.In
+					lifecycle fx.Lifecycle
+				},
+			) *pubsub.PubSub {
 				result := pubsub.New(32)
-				lifecycle.Append(fx.Hook{
-					OnStart: nil,
-					OnStop: func(ctx context.Context) error {
-						result.Shutdown()
-						return nil
+				params.lifecycle.Append(
+					fx.Hook{
+						OnStart: nil,
+						OnStop: func(ctx context.Context) error {
+							result.Shutdown()
+							return nil
+						},
 					},
-				})
+				)
 				return result
 			},
 		},
