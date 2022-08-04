357de8fddff78f49adb074d85b748129bfa535d1
Brendan Bosman <bhbosman@gmail.com>
2022-08-04 15:55:41 +0200

    refactor

--- model/PublishRxHandlerCounters.go
+++ model/PublishRxHandlerCounters.go
@@ -4,15 +4,21 @@ type PublishRxHandlerCounters struct {
 	ConnectionId string
 	Direction    StreamDirection
 	Counters     []*RxHandlerCounter
+	data         map[string]string
 }
 
 func NewPublishRxHandlerCounters(connectionId string, direction StreamDirection) *PublishRxHandlerCounters {
 	return &PublishRxHandlerCounters{
 		ConnectionId: connectionId,
 		Direction:    direction,
+		data:         make(map[string]string),
 	}
 }
 
 func (self *PublishRxHandlerCounters) Add(counter *RxHandlerCounter) {
 	self.Counters = append(self.Counters, counter)
+}
+
+func (self *PublishRxHandlerCounters) AddMapData(key string, value string) {
+	self.data[key] = value
 }
