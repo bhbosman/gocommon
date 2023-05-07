2561e27a2d2ff6e187dfa0cd639b0904368ba25f
Brendan Bosman <bhbosman@gmail.com>
2023-05-07 19:42:36 +0200

    WIP

--- observable.go
+++ observable.go
@@ -1,10 +1,55 @@
 package gocommon
 
-import "github.com/reactivex/rxgo/v2"
+import (
+	"github.com/reactivex/rxgo/v2"
+	"io"
+)
 
 type IObservable interface {
 	Observe(opts ...rxgo.Option) <-chan rxgo.Item
 	Map(apply rxgo.Func, opts ...rxgo.Option) rxgo.Observable
 	FlatMap(apply rxgo.ItemToObservable, opts ...rxgo.Option) rxgo.Observable
 	//rxgo.Observable
+}
+
+type ByteReaderCloser interface {
+	io.ByteReader
+	io.Closer
+}
+type byteReaderCloser struct {
+	br io.ByteReader
+}
+
+func (self *byteReaderCloser) ReadByte() (byte, error) {
+	return self.br.ReadByte()
+}
+
+func (self *byteReaderCloser) Close() error {
+	return nil
+}
+
+func NewByteReaderNoCloser(br io.ByteReader) ByteReaderCloser {
+	return &byteReaderCloser{
+		br: br,
+	}
+}
+
+type byteReaderWithCloser struct {
+	br io.ByteReader
+	c  io.Closer
+}
+
+func (self *byteReaderWithCloser) Close() error {
+	return self.c.Close()
+}
+
+func (self *byteReaderWithCloser) ReadByte() (byte, error) {
+	return self.br.ReadByte()
+}
+
+func NewByteReaderWithCloser(c io.Closer, br io.ByteReader) ByteReaderCloser {
+	return &byteReaderWithCloser{
+		br: br,
+		c:  c,
+	}
 }
